5f93f5fa0cdfecf6f9db444f3176b5b7f18baa76
agga1 <agud100@gmail.com>
2020-03-19 16:41:29 +0100

    added support for 2d grid graphs

--- lab2/time_eval.py
+++ lab2/time_eval.py
@@ -1,7 +1,14 @@
 from typing import Callable, List
 from time import perf_counter, time, process_time
 
-def time_eval(func, args: List, name: str, times=10):
+def time_eval(func: Callable, args: List, name="", times=10, print_res=False):
+    """
+    :param func: function to evaluate based on {@param times} runs
+    :param args: func arguments
+    :param name: function name
+    :param print_res: if true, prints first result
+    :return: result of func(args)
+    """
     start_pc = perf_counter()
     start_t = time()
     start_pt = process_time()
@@ -14,7 +21,9 @@ def time_eval(func, args: List, name: str, times=10):
     avg_pc = (end_pc-start_pc)/times
     avg_t = (end_t-start_t)/times
     avg_pt = (end_pt-start_pt)/times
-    print(res[0])
+    if print_res:
+        print(res[0])
     print(name, " running time actual: ", avg_t)
     print("process time: ", avg_pt)
     print("user time: ", avg_pc)
+    return res
--- lab3/zad1.py
+++ lab3/zad1.py
@@ -4,7 +4,11 @@ import numpy as np
 from typing import List, Callable, Tuple
 
 max_iter = 100000
-
+"""
+Aby znaleźć k-tą ilośc miejsc zerowych funkcji, musimy znaleźc k takich x, gdzie
+cosh(x) = 1/cos(x) -> funkcje te przetną się w okolicy cos(x)=0 -> między każdym n*pi, (n+1/2)pi
+czyli trzeba by robic bisekcję przedziałow (n*pi, (n+1/2)pi) gdzie n przyjmuje wartości od 0,..k-1.
+"""
 
 def bisection(f: Callable, prec: int, span: List, eps) -> Tuple[dec.Decimal, int]:
     """
@@ -43,6 +47,11 @@ def bisection(f: Callable, prec: int, span: List, eps) -> Tuple[dec.Decimal, int
 
 
 def expected_iter(span, eps):
+    """
+    :param span: [a, b]
+    :param eps: absolute error
+    :return: nr of expected iterations
+    """
     a, b = span
     expected_iter = math.ceil(np.log((a + b) / eps) / np.log(2))
     return expected_iter
