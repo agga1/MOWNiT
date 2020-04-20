2626caef85b188fc28b50201a845bd0055894f96
agga1 <agud100@gmail.com>
2020-04-20 13:36:15 +0200

    raw backend

---
+++ lab6/SEbackend/classes/Article.py
@@ -0,0 +1,13 @@
+from classes.Text import Text
+
+class Article:
+    """
+    Text with some metadata
+    """
+    def __init__(self, title: str, text: str, link: str = None):
+        self.title = title
+        self.text = Text(text)
+        self.link = link  # absolute path to the full article in txt form
+
+    def __repr__(self):
+        return f"title: {self.title}"  # link:{self.link}
