f80505c676d442abdba10307df94eb08e074eb2d
agga1 <agud100@gmail.com>
2020-04-23 22:23:37 +0200

    done..?

--- lab7/power_method.py
+++ lab7/power_method.py
@@ -1,37 +1,73 @@
+from typing import Callable, Tuple
+
 import numpy as np
 from scipy.linalg import eigh, lu_factor, lu_solve
 import matplotlib.pyplot as plt
 from time_eval import time_eval
 
-def power_method(A: np.array, max_iter: int = 100000, eps: float = 1e-8):
+
+def power_method(A: np.array, max_iter: int = 100000, eps: float = 1e-8, return_iter=False):
     """
     given a diagonalizable matrix A, the algorithm finds a number λ,
     which is the greatest (in absolute value) eigenvalue of A,
     and v - a corresponding (nonzero) eigenvector of λ.
     :param max_iter: algorithm stops at max_iter, or
     :param eps: after reaching ||xi−xi+1||< eps
-    :return λ, v
+    :param return_iter: returns nr of iterations as third element
+    :return λ, v [,iter]
     """
     x = np.random.rand(A.shape[0])
-    for i in range(max_iter):
+    i = 0
+    while i < max_iter:
+        i += 1
         x2 = A @ x
         x2 /= np.linalg.norm(x2)  # normalize
-        if np.linalg.norm(x2 - x) < eps:
-            print(f"{i} iter")
+        if np.linalg.norm(x2 - x) < eps or i+1==max_iter:
             break
         x = x2
 
     eig = x@A@x/(x@x)
-    # eig = np.max(np.abs(x))
+    x /= np.linalg.norm(x, axis=0)
+
+    if return_iter:
+        return eig, x, i
     return eig, x
 
+def inv_power_method(A: np.array, mu, max_iter: int = 100000, eps: float = 1e-8, return_iter=False):
+    """ finding eigen vector using inverse power method, and eigenvalue using Rayleigh quotient
+    :param A: matrix
+    :param mu: approximation for the eigenvalue corresponding to the desired eigenvector
+    :param max_iter: stop algorithm after @max_iter iterations
+    :param eps: stop algorithm after reaching desired accuracy
+    :param return_iter: returns nr of iterations as third element
+    :return: λ, v [,iter]
+    """
+    v0 = np.identity(A.shape[0])[0]
+    v, i = 0, 0
+    AuI = A - mu * np.identity(A.shape[0])
+    lupiv = lu_factor(AuI)
+    while i< max_iter:
+        i += 1
+        v = v0
+        w = lu_solve(lupiv, v)
+        v = w / np.linalg.norm(w)
+        if np.linalg.norm(v-v0) < eps or np.linalg.norm(v + v0) < eps:
+            break
+        mu = v.T @ A @ v
+        v0 = v
 
-def check(A, λ, v):
-    print(A @ v)
-    print(λ * v)
+    if return_iter:
+        return mu, v, i
+    return mu, v
+
+def improve_inv_method(A: np.array, *args):
+    mu, v, iter = power_method(A, max_iter=10, eps=1, return_iter=True)
+    print("foun mu ", mu," in ",  iter)
+    return inv_power_method(A, mu, *args)
 
 
-def check_with_library(n, func, eps=1e5):
+
+def check_with_library(n, func: Callable[[np.array, any], Tuple[float, np.array]], eps=1e-5):
     A = np.random.rand(n, n)
     A = A.T @ A
     _ , [eig, v] = time_eval(func, "My power method", 1, A)
@@ -42,13 +78,16 @@ def check_with_library(n, func, eps=1e5):
     if abs(eig_lib - eig) > eps:
         print("wrong eigen value")
         return
+
     for idx in range(len(v)):
-        if abs(v[idx] - v_lib[idx]) > eps:
+        if abs(abs(v[idx]) - abs(v_lib[idx])) > eps:
             print("wrong eigen vector")
             return
     print("Algorithm correct!\n")
 
-def summary():
+
+def plot_power_method():
+    """ plots correlation between time needed for power_method execution and input matrix size"""
     xs = []
     ys = []
     for k in range(100, 2500, 100):
@@ -60,31 +99,28 @@ def summary():
     plt.plot(xs, ys)
     plt.show()
 
-def inverse_power_iteration(A, u, max_iters, epsilon):
-    v0 = np.identity(A.shape[0])[0]
-    AuI = A - u*np.identity(A.shape[0])
-    lupiv = lu_factor(AuI)
-    for i in range(max_iters):
-        v = v0
-        w = lu_solve(lupiv, v)
-        v = w / np.linalg.norm(w)
-        if np.linalg.norm(v-v0) < epsilon or np.linalg.norm(v+v0) < epsilon:
-            print(f"{i} iter")
-            return u, v
-        u = v.T @ A @ v
-        v0 = v
+# checking power_method correctness using library functions
+check_with_library(100, power_method, 1e-5)
+check_with_library(300, power_method, 1e-5)
 
+# plotting correlation between time needed for power_method execution and input matrix size
+plot_power_method()
 
-A = np.random.rand(10, 10)
+# comparing
+A = np.random.rand(100, 100)
 A = A * np.transpose(A)
 
-# eigval, eigvec= inverse_power_iteration(A, 0, 100000, 1e-8)
-# print(eigvec)
-eigval, eigvec = power_method(A, 100000, 1e-8)
-print(eigvec)
+eigvec, eigval , iter = inv_power_method(A, 0, 1000, 1e-8, return_iter=True)
+eigvec2, eigval2, iter2 = inv_power_method(A, 10, 1000, 1e-8, return_iter=True)
+eigvec3, eigval3, iter3 = inv_power_method(A, 55, 1000, 1e-8, return_iter=True)
+eigvec4, eigval4, iter4 = power_method(A, 1000, 1e-5, return_iter=True)
+mu, _ = power_method(A, max_iter=10, eps=0.3)  # assuming we know a good approximation of eigenvalue
+eigvec5, eigval5, iter5 = inv_power_method(A, mu, 1000, 1e-8, True)
 
-check_with_library(100, power_method, 1e5)
-check_with_library(1000, power_method, 1e5)
-# summary()
+print(eigvec, eigval[0], iter)
+print(eigvec2, eigval2[0], iter2)
+print(eigvec3, eigval3[0], iter3)
+print(eigvec4, eigval4[0], iter4)
+print(eigvec5, eigval5[0], iter5)
 
 
