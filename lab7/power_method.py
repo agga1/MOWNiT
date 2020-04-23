69cd362abfbc93abc9fab40be43249b1b03e393f
agga1 <agud100@gmail.com>
2020-04-23 18:01:21 +0200

    lab7

--- lab7/power_method.py
+++ lab7/power_method.py
@@ -1,9 +1,9 @@
 import numpy as np
-from scipy.linalg import eigh
+from scipy.linalg import eigh, lu_factor, lu_solve
 import matplotlib.pyplot as plt
 from time_eval import time_eval
 
-def power_method(A: np.array, max_iter: int = 500000, eps: float = 1e-8):
+def power_method(A: np.array, max_iter: int = 100000, eps: float = 1e-8):
     """
     given a diagonalizable matrix A, the algorithm finds a number λ,
     which is the greatest (in absolute value) eigenvalue of A,
@@ -12,12 +12,12 @@ def power_method(A: np.array, max_iter: int = 500000, eps: float = 1e-8):
     :param eps: after reaching ||xi−xi+1||< eps
     :return λ, v
     """
-    x = np.ones(A.shape[0])
-    for _ in range(max_iter):
+    x = np.random.rand(A.shape[0])
+    for i in range(max_iter):
         x2 = A @ x
         x2 /= np.linalg.norm(x2)  # normalize
         if np.linalg.norm(x2 - x) < eps:
-            # print("accuracy reached")
+            print(f"{i} iter")
             break
         x = x2
 
@@ -31,10 +31,10 @@ def check(A, λ, v):
     print(λ * v)
 
 
-def check_with_library(n, eps=1e5):
+def check_with_library(n, func, eps=1e5):
     A = np.random.rand(n, n)
     A = A.T @ A
-    _ , [eig, v] = time_eval(power_method, "My power method", 1, A)
+    _ , [eig, v] = time_eval(func, "My power method", 1, A)
 
     _, [eig_lib, v_lib] = time_eval(eigh, "Library function", 1, A, eigvals=(n - 1, n - 1))
     eig_lib, v_lib = eig_lib[0], np.transpose(v_lib)[0]
@@ -60,8 +60,31 @@ def summary():
     plt.plot(xs, ys)
     plt.show()
 
-check_with_library(100, 1e5)
-check_with_library(1000, 1e5)
-summary()
+def inverse_power_iteration(A, u, max_iters, epsilon):
+    v0 = np.identity(A.shape[0])[0]
+    AuI = A - u*np.identity(A.shape[0])
+    lupiv = lu_factor(AuI)
+    for i in range(max_iters):
+        v = v0
+        w = lu_solve(lupiv, v)
+        v = w / np.linalg.norm(w)
+        if np.linalg.norm(v-v0) < epsilon or np.linalg.norm(v+v0) < epsilon:
+            print(f"{i} iter")
+            return u, v
+        u = v.T @ A @ v
+        v0 = v
+
+
+A = np.random.rand(10, 10)
+A = A * np.transpose(A)
+
+# eigval, eigvec= inverse_power_iteration(A, 0, 100000, 1e-8)
+# print(eigvec)
+eigval, eigvec = power_method(A, 100000, 1e-8)
+print(eigvec)
+
+check_with_library(100, power_method, 1e5)
+check_with_library(1000, power_method, 1e5)
+# summary()
 
 
