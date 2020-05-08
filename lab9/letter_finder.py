36a744c90a8e417934da307cd1e52d0a4d674ed2
agga1 <agud100@gmail.com>
2020-05-08 22:42:06 +0200

    noise reduction

--- lab9/letter_finder.py
+++ lab9/letter_finder.py
@@ -1,6 +1,7 @@
-from operator import itemgetter
-
+import numpy as np
+import matplotlib.pyplot as plt
 from numpy import fft
+from math import trunc
 from other_parse import parse_alphabet, parse_letters, parse_text, flipped_fourier, find_letters, clean_duplicates, circle, \
     order
 
@@ -17,11 +18,27 @@ def to_str(letter_order, letter_shape):
     print(line)
     return line
 
+def reduce_noise(matrix, k):
+    U, S, V = np.linalg.svd(matrix, full_matrices=False)  # singular value decomposition
+    smat = np.diag(S)
+    smat[k:, k:] = 0
+    print(U.shape, S.shape, V.shape, smat.shape)
+    return np.dot(U, np.dot(smat, V))
+
+def reduce_noise2(matrix, k):
+    U, S, V = np.linalg.svd(matrix, full_matrices=True)  # singular value decomposition
+    Uvcts = np.matrix(U[:, :k])  # first k-t singular vectors
+    Svals = np.diag(S[:k])  # k first singular values
+    Vvcts = np.matrix(V[:k, :])  # transposed k-th vct = k-th column
+    return Uvcts * Svals * Vvcts
+
 def find_all_letters(text_name, alphabet_name):
     alphabet = parse_alphabet(alphabet_name, 2, 5, 7, 2)
     letters = parse_letters(alphabet)
     text = parse_text(text_name)
-
+    text = reduce_noise2(text, trunc(50))
+    plt.imshow(text, cmap="gray")
+    plt.show()
     # perform fourier transform
     letters_f = flipped_fourier(letters, text.shape)
     text_f = fft.fft2(text)
@@ -43,7 +60,7 @@ def find_all_letters(text_name, alphabet_name):
 
 # consolas150 # 2 5 7 2
 # cn150 2 1 3 2
-text_name = "res/consolas150.png"
+text_name = "res/consolas150noise.png"
 
 alphabet_name = "res/consolas_alph.png"
 find_all_letters(text_name, alphabet_name)
