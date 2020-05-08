684bcd702867629d465b0d468ab7dfd70a4c2b5e
agga1 <agud100@gmail.com>
2020-05-08 21:48:00 +0200

    added sans-sherif font

--- lab9/letter_finder.py
+++ lab9/letter_finder.py
@@ -14,10 +14,11 @@ def to_str(letter_order, letter_shape):
         if letter_order[i][0][1] - letter_order[i - 1][0][1] > letter_shape[1] + 3:
             line += " "
         line += letter_order[i][1]
+    print(line)
     return line
 
 def find_all_letters(text_name, alphabet_name):
-    alphabet = parse_alphabet(alphabet_name, 2, 1, 3, 2)
+    alphabet = parse_alphabet(alphabet_name, 2, 5, 7, 2)
     letters = parse_letters(alphabet)
     text = parse_text(text_name)
 
@@ -26,7 +27,7 @@ def find_all_letters(text_name, alphabet_name):
     text_f = fft.fft2(text)
     print(letters[0].shape)
     positions = find_letters(letters_f, text_f, thres=0.93)
-    clean_duplicates(positions, letters[0].shape, margin=5)
+    clean_duplicates(positions, letters[0].shape, margin=7)
 
     # for i in range(10, 25):
     #     circle(text_name, positions, letters[0].shape, [i])
@@ -40,6 +41,9 @@ def find_all_letters(text_name, alphabet_name):
     to_str(letter_order, letters[0].shape)
 
 
-text_name = "res/cn150_2.png"
-alphabet_name = "res/cn_sep_b.png"
+# consolas150 # 2 5 7 2
+# cn150 2 1 3 2
+text_name = "res/consolas150.png"
+
+alphabet_name = "res/consolas_alph.png"
 find_all_letters(text_name, alphabet_name)
